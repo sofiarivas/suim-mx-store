81f2d3b2fa18fa54e7858cbc63f4a2727703f78b
Sofi Rivas <sofi.rivasm@gmail.com>
2017-06-09 11:36:57 -0500

    Add slick component to App

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,24 @@
     <mailing-form class="mailing-container"></mailing-form>
     <brands-display></brands-display>
     <social-media-footer></social-media-footer>
+    <slick ref="slick" :options="slickOptions">
+      <a href="http://placehold.it/200x200"><img src="http://placehold.it/200x200" alt=""></a>
+      <a href="http://placehold.it/200x200"><img src="http://placehold.it/200x200" alt=""></a>
+      <a href="http://placehold.it/200x200"><img src="http://placehold.it/200x200" alt=""></a>
+      <a href="http://placehold.it/200x200"><img src="http://placehold.it/200x200" alt=""></a>
+      <a href="http://placehold.it/200x200"><img src="http://placehold.it/200x200" alt=""></a>
+      <a href="http://placehold.it/200x200"><img src="http://placehold.it/200x200" alt=""></a>
+      <a href="http://placehold.it/200x200"><img src="http://placehold.it/200x200" alt=""></a>
+      <a href="http://placehold.it/200x200"><img src="http://placehold.it/200x200" alt=""></a>
+      <a href="http://placehold.it/200x200"><img src="http://placehold.it/200x200" alt=""></a>
+      <a href="http://placehold.it/200x200"><img src="http://placehold.it/200x200" alt=""></a>
+      <a href="http://placehold.it/200x200"><img src="http://placehold.it/200x200" alt=""></a>
+    </slick>
   </div>
 </template>
 
 <script>
+import Slick from 'vue-slick';
 import SuimHeader from './components/SuimHeader';
 import MailingForm from './components/MailingForm';
 import BrandsDisplay from './components/BrandsDisplay';
@@ -20,6 +34,43 @@ export default {
     MailingForm,
     BrandsDisplay,
     SocialMediaFooter,
+    Slick,
+  },
+  data() {
+    return {
+      slickOptions: {
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true,
+            },
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            },
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            },
+          },
+        ],
+      },
+    };
   },
 };
 </script>
